c53476bc17c04c2ba7fe2399c9ac0c3e9a219462
SamatAlim <samatalimbaev@mail.ru>
2023-10-25 01:01:50 +0600

    comment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ToDo from "./ToDo";
+import App from './App';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ToDo />
+    <App />
   </React.StrictMode>
 );
 
